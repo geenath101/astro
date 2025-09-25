da17c3c28c8fdf174fb09651ed3826bc1ee84646
Geenath <geenathdl024@gmail.com>
2025-09-25 14:32:31 +1200

    code defactored and updated the shooting logic

---
+++ astro/shot.py
@@ -0,0 +1,13 @@
+from circleshape import *
+from constants import *
+
+
+class Shot(CircleShape):
+    def __init__(self,x,y,radius):
+        super().__init__(x,y,radius)
+    def draw(self,screen):
+        #print("shooooting")
+        pygame.draw.circle(screen,"white",self.position,2)
+    def update(self,dt):
+        print("updating")
+        self.position = self.position + self.velocity * dt
